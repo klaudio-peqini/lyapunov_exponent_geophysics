419422b380d9083bddb71b621554a5d782afccd4
Klaudio-Peqini <154642975+Klaudio-Peqini@users.noreply.github.com>
2026-06-20 00:19:37 +0200

    Delete src/lyapunov_geo directory

--- src/lyapunov_geo/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-lyapunov_geo
-
-Package providing Lyapunov exponent tools for time series and standard toy-model maps.
-"""
-from .lyapunov import SeriesLyapunovConfig, lyapunov_exponent_series
